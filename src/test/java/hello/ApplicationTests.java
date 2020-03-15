de4f5eb4f9e8332d58fe45b42b8efe653dacb4db
Karthikeyan B <nklkarthi@gmail.com>
2020-03-15 13:06:57 +0800

    Update package as per gs-spring-boot-docker

--- src/test/java/hello/ApplicationTests.java
+++ src/test/java/hello/ApplicationTests.java
@@ -1,10 +1,10 @@
-package org.springframework.gsspringbootdocker;
+package hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
